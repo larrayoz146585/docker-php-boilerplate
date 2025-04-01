4baf0ce2c11d424045b801f55e40060f76a4be91
larrayoz146585 <148328956+larrayoz146585@users.noreply.github.com>
2025-04-01 13:41:34 +0200

    [refactor] - Algunos espacios y saltos de linea

--- src/ListaCompra.php
+++ src/ListaCompra.php
@@ -4,26 +4,15 @@ namespace Deg540\DockerPHPBoilerplate;
 
 class ListaCompra
 {
+
     // TODO: Lista de la compra Kata
     public static function listaCompra(string $productos): string
     {
-        /*
-        * Añadir productos
-        ●  Instrucción: añadir <nombre> [cantidad]
-
-        ●  Si no se indica cantidad, se asume 1.
-        ●  Si el producto ya existe en la lista, se suma la nueva cantidad a la anterior.
-        ●  Ejemplos:
-
-        ○  añadir pan → "pan x1"
-
-        ○  añadir Pan 2 → "pan x3"
- */
         if($productos[2] != "") {
             $producto = explode(" ", $productos);
             $nombre = strtolower($producto[1]);
             $cantidad = (int)$producto[2];
-            return $nombre . " x" . ($cantidad)
+            return $nombre . " x" . ($cantidad);
         } else if ($productos[2] == "") {
             $producto = explode(" ", $productos);
             $nombre = strtolower($producto[1]);
