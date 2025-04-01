770cd7b478ada3a23c105e458ff976b44b5ffc6e
larrayoz146585 <148328956+larrayoz146585@users.noreply.github.com>
2025-04-01 12:21:09 +0200

    [#] - Primer comit de prueba

---
+++ tests/ListaCompraTests.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Deg540\DockerPHPBoilerplate\Test;
+
+use Deg540\DockerPHPBoilerplate\ListaCompra;
+use PHPUnit\Framework\TestCase;
+
+class ListaCompraTests extends TestCase
+{
+    /**
+     * @test
+     **/
+    public function testName()
+    {
+
+    }
+
+}
